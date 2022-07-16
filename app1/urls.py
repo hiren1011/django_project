8fb3f1be28eaa0c01727f1498fc9401a36d98e73
hiren <91483047+hiren1011@users.noreply.github.com>
2022-07-16 18:54:06 +0530

    Update urls.py

--- app1/urls.py
+++ app1/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('products/<int:no>',product_view,name='product_url'),
     path('edit-product/<int:no>',edit_product_view,name='edit_product'), 
     path('delete-product/<int:no>',delete_product_view,name='delete_product'), 
-    path('delete-product/<int:no>/?delete=true',delete_product_view,name='delete_product'), 
+    path('products/delete-product/<int:no>',delete_product_view,name='delete_product'),  
     path('add-product/',add_product_view,name='add_product'),
 
     path('logout/',logout_view,name='logout')
